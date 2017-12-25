3703f4afff7c5c03cb6224922e9b7ba443b2f94d
Noelia-F <noelia.fronton.g@gmail.com>
2017-12-25 20:41:35 +0100

    events in the popup

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
 'use strict';
-var circunference =document.querySelector('.circunference')
-
+var circunference =document.querySelector('.circunference');
+var popup = document.querySelector('.popup');
+var info = document.querySelector('.container--info');
+var buttonInfo = document.querySelector('.button--info');
+var close = document.querySelector('.container--close');
+var buttonClose = document.querySelector('.button--close');
 function changeColor(){
 	//cambiar color
 	var computedStyle = getComputedStyle(this);
@@ -14,3 +18,34 @@ function addListeners (){
   }
 }
 addListeners();
+
+//Popup que desaparece pasados 30 segundos.
+
+function disappearPopup() {
+	popup.classList.add('hidden');
+}
+
+//aparece un botón para acceder de nuevo a la información del popup.
+
+function appearButton() {
+	info.classList.remove('hidden');
+}
+
+//posibilidad de hacer click y que aparezca de nuevo la información
+function appearPopup() {
+	popup.classList.remove('hidden');
+	info.classList.add('hidden');
+	close.classList.remove('hidden');
+}
+
+//cerrar el popup con la información de cómo se juega
+function closePopup() {
+	popup.classList.add('hidden');
+	info.classList.remove('hidden');
+	close.classList.add('hidden');
+}
+setTimeout(disappearPopup, 3000);
+setTimeout(appearButton, 3000);
+console.log(buttonInfo);
+buttonInfo.addEventListener('click', appearPopup);
+buttonClose.addEventListener('click', closePopup);
