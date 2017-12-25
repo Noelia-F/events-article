43513f49eeba88b39469332c865af3065244dfd1
Cris <crisgalasanchez@gmail.com>
2017-12-25 23:44:48 +0100

    click and mouseover function

--- js/main.js
+++ js/main.js
@@ -1,16 +1,26 @@
 'use strict';
 var circunference =document.querySelector('.circunference')
+var lastColor = getComputedStyle(circunference).backgroundColor;
 
 function changeColor(){
-	//cambiar color
+	var computedStyle = getComputedStyle(this);
+	lastColor = computedStyle.backgroundColor;
+	circunference.style.backgroundColor= computedStyle.backgroundColor;
+}
+function showColor(){
+	lastColor = getComputedStyle(circunference).backgroundColor;
 	var computedStyle = getComputedStyle(this);
 	circunference.style.backgroundColor= computedStyle.backgroundColor;
+}
+function restoreColor(){
+	circunference.style.backgroundColor = lastColor;
 }
 function addListeners (){
 	var buttons = document.querySelectorAll('.button');
 	for (var i=0;i < buttons.length; i++){
 		buttons[i].addEventListener('click',changeColor);
-		buttons[i].addEventListener('mouseover',changeColor);
+		buttons[i].addEventListener('mouseover',showColor);
+		buttons[i].addEventListener('mouseout',restoreColor);
   }
 }
 addListeners();
